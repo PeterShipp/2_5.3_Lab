8ea77509fedd33a67182b086535cdc3ca5d550b2
PeterShipp <pshipp81@aol.com>
2020-02-18 14:01:31 -0800

    Add project files.

---
+++ 2_5.3_Lab/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _2_5._3_Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter your Age");
+
+            int age;
+            age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("You look younger than " +age);
+
+            
+        }
+    }
+}
